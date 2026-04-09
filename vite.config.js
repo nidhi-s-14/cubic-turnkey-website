01266367e9ffab25c58fedd37d9aac87bbeaa88c
Nidhi Sura <nidhi.sura1999@gmail.com>
2026-04-09 15:04:26 -0700

    fix: Configured base path correctly for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+const githubPagesBase = "/cubic-turnkey-website/";
+
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? githubPagesBase : "/",
   plugins: [react()],
-});
+}));
